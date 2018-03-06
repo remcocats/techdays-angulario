0e94e85c06386d90e670bd092c47d5f8f8013291
JWA van Golverdinge Schut <Jaron.van.Golverdinge.Schut@rabobank.nl>
2018-03-06 23:18:31 +0100

    Added step 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EventService} from './event.service';
+import {ChangeDetectorRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public title = 'app';
+  public eventList = new Array();
+
+  constructor(private ref: ChangeDetectorRef, private eventService: EventService) {}
+
+  ngOnInit() {
+    this.eventService.getEvents('http://localhost/api').subscribe(event => {
+      console.log(event);
+      this.eventList.push(event);
+      this.ref.detectChanges();
+    });
+  }
 }
